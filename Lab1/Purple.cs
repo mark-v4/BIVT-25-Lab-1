2264fa40fb56010e3ce4b54f9fc42fb44b738aa6
mark-v4 <markovkaplay101@gmail.com>
2025-09-11 02:36:37 +0300

    Purple solved

--- Lab1/Purple.cs
+++ Lab1/Purple.cs
@@ -8,6 +8,15 @@
 
             // code here
 
+            if (a * b > 0 && b * c > 0)
+            {
+                answer = true;
+            }
+            else if (a == 0 && b == 0 && c == 0)
+            {
+                answer = true;
+            }
+
             // end
 
             return answer;
@@ -17,6 +26,11 @@
             bool answer = false;
 
             // code here
+
+            if (b != 0 && a % b == 0 || a != 0 && b % a == 0)
+            { 
+                answer = true;
+            }
 
             // end
 
@@ -28,6 +42,11 @@
 
             // code here
 
+            if (a * a == b || b * b == a || a * a * a == b || b * b * b == a)
+            {
+                answer = true;
+            }
+
             // end
 
             return answer;
@@ -37,6 +56,9 @@
             double answer = 0;
 
             // code here
+
+            var dis = f * f - 4 * d * g;
+            answer = dis;
 
             // end
 
@@ -48,15 +70,34 @@
 
             // code here
 
-            // end
+            if (x <= -1)
+            {
+                answer = 1;
+            }
+            else if (x > 1)
+            {
+                answer = -1;
+            } else
+            {
+                answer = -x;
+            }
 
-            return answer;
+                // end
+
+                return answer;
         }
         public bool Task6(double squareS, double circleS)
         {
             bool answer = false;
 
             // code here
+
+            var r = circleS / Math.PI;
+            var d = squareS / 2;
+            if (d < r)
+            {
+                answer = true;
+            }
 
             // end
 
@@ -69,9 +110,45 @@
 
             // code here
 
-            // end
+            if (s)
+            {
+                if (t)
+                {
+                    answer = 6;
+                }
+                else
+                {
+                    if (f)
+                    {
+                        answer = 10;
+                    }
+                    else
+                    {
+                        answer = 2;
+                    }
+                }
+            }
+            else
+            {
+                if (t)
+                {
+                    answer = 3;
+                }
+                else
+                {
+                    if (f)
+                    {
+                        answer = 5;
+                    }
+                    else
+                    {
+                        answer = 1;
+                    }
+                }
+            }
+                // end
 
-            return answer;
+                return answer;
         }
         public bool Task8(int year, int pupils, int salary)
         {
@@ -79,6 +156,17 @@
             const int bank = 10000;
 
             // code here
+
+            int avr = (int)Math.Ceiling((double)pupils / 7);
+            Console.WriteLine(avr);
+            int sum = avr * salary + 5 * pupils;
+            bool p1 = sum <= bank;
+            bool p2 = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+            if (p1 && !p2)
+            {
+                answer = true;
+            }
             
             // end
 
